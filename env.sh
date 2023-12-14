2af7144655c56d14a5855f1c2c263494f6468c8f
Lucas04 <117806313+Lucas04-nhr@users.noreply.github.com>
2023-12-14 17:09:37 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,19 @@
+#! /bin/zsh
+
+echo "Do you want to set the environment variables for WEScall? (y/n)"
+read -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+ then
+   echo "Setting up environment variables for WEScall..."
+   export PL_DIR=$HOME/WEScall
+
+  if [[ -d "$HOME/WEScall/data" ]]
+    then
+      export WK_DIR=$HOME/WEScall/data
+  else
+      mkdir  "$HOME/WEScall/data"
+      export WK_DIR=$HOME/WEScall/data
+  fi
+fi
+
